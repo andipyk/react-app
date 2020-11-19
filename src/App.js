02604017bf7cceb58f35ea6a1b50fa3ed906a1dc
Andi Syafrianda <andi.syafrianda@aux.go-jek.com>
2020-11-19 15:05:42 +0700

    create props & first component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Pesan from "./components/Pesan";
+import BuatPesan from "./components/BuatPesan";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My React</h1>
+      <Pesan nama="andi" pesan="halo gaes" />
+      <Pesan nama="budi" pesan="yoi" />
+      <Pesan nama="cristo" pesan="tess" />
     </div>
   );
 }
---
+++ src/components/Pesan.js
@@ -0,0 +1,10 @@
+const Pesan = ({ nama, pesan }) => {
+  return (
+    <div>
+      <h2>Nama: {nama}</h2>
+      <p>Pesan: {pesan}</p>
+    </div>
+  );
+};
+
+export default Pesan;
