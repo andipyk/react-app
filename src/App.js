bcdc8445201c82a736677bcf7048c7869d334225
Andi Syafrianda <andi.syafrianda@aux.go-jek.com>
2020-11-23 18:22:06 +0700

    simple mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-// import { useState } from "react";
+import { useState, useEffect } from "react";
 import List from "./components/List";
 import "./styles.css";
 
 function App() {
+  const API_URL = "https://jsonplaceholder.typicode.com/users";
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = async () => {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    setMyData(data);
+    console.log(myData[0]);
+  };
+
+  const [myData, setMyData] = useState([]);
+
   return (
     <div className="App">
       <List />
+      <ol>
+        {myData.map((v) => (
+          <li>{v.name}</li>
+        ))}
+      </ol>
     </div>
   );
 }
--- src/components/Filter.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export default ({filter, setFilter}) => {
-  const handleChange = event => {setFilter(event.target.value)}
-  return <input type='text' value={filter} onChange={handleChange} placeholder='Filter'/>
-}
